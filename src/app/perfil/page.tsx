cd222e15a3ef63e9810b8b9906112625a66af9f7
ztiagok <ztiagok@gmail.com>
2023-07-18 12:03:25 -0300

    refactor: Alterando div para main nas configurações / Alterando parametros do perfil para 'steamid' / Aumentando quantidade de Skeletons na Home

--- src/app/perfil/page.tsx
+++ src/app/perfil/page.tsx
@@ -1,22 +1,28 @@
 'use client'
+// import AllSkins from '@/components/Skins/AllSkins'
 import ChoiceItems from '@/components/Others/ChoiceItems'
 import PerfilPerson from '@/components/Others/ProfilePerson'
-// import AllSkins from '@/components/Skins/AllSkins'
-// import RentedSkins from '@/components/Skins/RentedSkins'
-// import useComponentStore from '@/stores/components.store'
+import useUserStore from '@/stores/user.store'
+import { useParams, useRouter } from 'next/navigation'
+import { useEffect } from 'react'
 
-export default function Perfil() {
-  // const { profileTabValue } = useComponentStore()
+export default function PerfilUsuario() {
+  const { steamid } = useParams()
+  const router = useRouter()
+
+  const { user } = useUserStore()
+
+  useEffect(() => {
+    if (steamid === user.steamid) {
+      router.push('/perfil')
+    }
+  }, [steamid, user, router])
 
   return (
     <main className="mx-auto flex w-4/5 flex-col items-center py-7">
-      <PerfilPerson />
-      <ChoiceItems thereIsRented={true} />
-      {/* {profileTabValue === 'sales' ? (
-          // <AllSkins itemsPerPage={15} />
-        ) : (
-          <RentedSkins />
-        )} */}
+      <PerfilPerson isSeller={false} />
+      <ChoiceItems />
+      {/* <AllSkins skinsCategories={} itemsPerPage={15} /> */}
     </main>
   )
 }
