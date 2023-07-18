c6c49db06cf816468842b0bdc335d36e32eb01cd
ztiagok <ztiagok@gmail.com>
2023-07-18 12:07:01 -0300

    refactor: Criando novo método de logout do Authorization / Corrigindo verificação do LocalStorage

--- src/tools/localstorage.tool.ts
+++ src/tools/localstorage.tool.ts
@@ -6,7 +6,7 @@ export default class LocalStorage {
   public static get(key: string) {
     const response = localStorage.getItem(key)
 
-    if (response !== null || response !== undefined) {
+    if (response !== null && response !== undefined && response !== '') {
       return JSON.parse(response as string)
     }
 
