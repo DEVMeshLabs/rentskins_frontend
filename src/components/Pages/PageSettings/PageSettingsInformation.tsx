7e6d457e7145a31ad2410ee9453e4eadac720a54
Tiago <ztiagok@gmail.com>
2023-07-26 19:58:42 -0300

    feat: Adicionando input de telefone nas opções de Configurações

--- src/components/Pages/PageSettings/PageSettingsInformation.tsx
+++ src/components/Pages/PageSettings/PageSettingsInformation.tsx
@@ -3,7 +3,8 @@ import Common from '@/components/Common'
 import Form from '@/components/Forms'
 import { IconClose } from '@/components/Icons'
 import { useForm } from 'react-hook-form'
-import { formResolver } from './schemas/information.schema'
+import { formResolver as contactResolver } from './schemas/information.contact.schema'
+import { formResolver as personalResolver } from './schemas/information.personal.schema'
 
 export function PageSettingsInformation() {
   const {
@@ -13,13 +14,29 @@ export function PageSettingsInformation() {
     register,
     formState: { errors },
   } = useForm({
-    resolver: formResolver,
+    resolver: personalResolver,
     defaultValues: {
       'trade-link': undefined,
     },
   })
 
-  const onSubmit = (data: any) => {
+  const {
+    handleSubmit: handleSubmit2,
+    register: register2,
+    formState: { errors: errors2 },
+  } = useForm({
+    resolver: contactResolver,
+    defaultValues: {
+      email: undefined,
+      phone: undefined,
+    },
+  })
+
+  const onSubmitPersonal = (data: any) => {
+    console.log(data)
+  }
+
+  const onSubmitContact = (data: any) => {
     console.log(data)
   }
 
@@ -41,7 +58,7 @@ export function PageSettingsInformation() {
         </div>
 
         <Form.Root
-          onSubmit={handleSubmit(onSubmit)}
+          onSubmit={handleSubmit(onSubmitPersonal)}
           className="mt-8 flex flex-col gap-2"
         >
           <Common.Title size={'lg'} color="white" className="-mt-4 mb-4">
@@ -103,24 +120,79 @@ export function PageSettingsInformation() {
       </div>
 
       {/* Bottom */}
-      <div className="rounded-2xl bg-mesh-color-neutral-800 px-4 py-6">
+      <Form.Root
+        onSubmit={handleSubmit2(onSubmitContact)}
+        className="rounded-2xl bg-mesh-color-neutral-800 px-4 py-6"
+      >
         <Common.Title bold={700} size={'2xl'} color="white">
           Informações de Contato
         </Common.Title>
-        <div className="mt-8 flex items-center justify-between">
-          <div className="flex flex-col">
-            <Common.Title size={'lg'} color="white">
-              Email
-            </Common.Title>
-            <span className="text-mesh-color-neutral-200">
-              https://rentskins/?sellerid=10902554 (MAKE IT FUNCTIONAL)
-            </span>
+        <div className="mt-8 flex flex-col items-start justify-center">
+          <Common.Title size={'lg'} color="white" className="-mt-4 mb-4">
+            Email
+          </Common.Title>
+          <div className="flex w-full items-center justify-between">
+            <Form.Input.Text
+              labelClassName="w-7/12 text-white mb-4"
+              placeholder="exemplo@email.com"
+              name="email"
+              register={register2('email')}
+              className={`rounded-md bg-mesh-color-neutral-600 px-3 py-2
+            transition-all`}
+              errors={errors2.email}
+              errorsClassname="text-red-500 text-sm mt-8 absolute"
+            />
+
+            <div className="flex w-3/12 justify-evenly">
+              <Form.Button
+                buttonStyle={undefined}
+                onSubmit={handleSubmit2(onSubmitContact)}
+                className="-mt-8 border-none text-mesh-color-primary-1200 opacity-70 hover:opacity-100"
+              >
+                Editar
+              </Form.Button>
+              <Common.Button
+                tabIndex={-1}
+                className="-mt-8 border-none text-mesh-color-primary-1200 opacity-70 hover:opacity-100"
+              >
+                Copiar Link
+              </Common.Button>
+            </div>
           </div>
-          <Common.Button className="border-none text-mesh-color-primary-1200 opacity-70 hover:opacity-100">
-            Copiar Link
-          </Common.Button>
         </div>
-      </div>
+        <div className="mt-8 flex flex-col items-start justify-center">
+          <Common.Title size={'lg'} color="white" className="-mt-4 mb-4">
+            Telefone
+          </Common.Title>
+          <div className="flex w-full items-center justify-between">
+            <Form.Input.Phone
+              labelClassName="w-7/12 text-white mb-4"
+              placeholder="(00) 00000-0000"
+              name="email"
+              register={register2('phone')}
+              inputClassName={`rounded-md bg-mesh-color-neutral-600 px-3 py-2
+              transition-all`}
+              errors={errors2.phone}
+              errorsClassname="text-red-500 text-sm mt-8 absolute"
+            />
+            <div className="flex w-3/12 justify-evenly">
+              <Form.Button
+                buttonStyle={undefined}
+                onSubmit={handleSubmit2(onSubmitContact)}
+                className="-mt-8 border-none text-mesh-color-primary-1200 opacity-70 hover:opacity-100"
+              >
+                Editar
+              </Form.Button>
+              <Common.Button
+                tabIndex={-1}
+                className="-mt-8 border-none text-mesh-color-primary-1200 opacity-70 hover:opacity-100"
+              >
+                Copiar Link
+              </Common.Button>
+            </div>
+          </div>
+        </div>
+      </Form.Root>
     </div>
   )
 }
