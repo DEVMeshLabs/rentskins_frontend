15385a266794fb309214dd907b40b07feaf53f2a
ztiagok <ztiagok@gmail.com>
2023-08-28 16:00:18 -0300

    refactor: Melhorando o modal de edição da skin

--- src/components/Modal/ModalSkinShowcase/ModalSkinShowcaseMain.tsx
+++ src/components/Modal/ModalSkinShowcase/ModalSkinShowcaseMain.tsx
@@ -38,14 +38,14 @@ export function ModalSkinShowcaseMain({
     <Dialog.Root>
       <Dialog.Trigger asChild>{activator}</Dialog.Trigger>
       <Dialog.Portal>
-        <Dialog.Overlay className="fixed inset-0  flex bg-black/70" />
+        <Dialog.Overlay className="fixed inset-0 z-20 flex bg-black/70 transition-all" />
         <Dialog.Content
-          className="fixed left-1/2 top-1/2 z-40 h-[80%] w-[85%] -translate-x-1/2
-        -translate-y-1/2 rounded-2xl bg-mesh-color-neutral-700 pb-10"
+          className="fixed left-1/2 top-1/2 z-50 flex h-[75%] w-[80%]
+        -translate-x-1/2 -translate-y-1/2 flex-col justify-center rounded-2xl bg-mesh-color-neutral-700"
         >
-          <div className="flex h-full w-full flex-col items-center justify-between pt-8">
+          <div className="flex h-full w-full flex-col items-center justify-between p-4 ">
             {/* TOP */}
-            <div className="flex w-11/12 items-center justify-between">
+            <div className="flex w-full items-center justify-between">
               <Dialog.Title>
                 {isEdition && (
                   <Common.Title bold={600} size="2xl" color="white">
@@ -54,7 +54,7 @@ export function ModalSkinShowcaseMain({
                 )}
               </Dialog.Title>
               <Dialog.Close asChild>
-                <Common.Button className="border-transparent outline-none">
+                <Common.Button className="-mb-6 border-transparent outline-none">
                   <IconClose />
                 </Common.Button>
               </Dialog.Close>
