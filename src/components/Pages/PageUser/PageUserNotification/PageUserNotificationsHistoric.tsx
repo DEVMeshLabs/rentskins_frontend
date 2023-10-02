55e01184fae15d34adfc198e36e10a31d61b901a
Gabriel Almeida <viniciusgsa99@gmail.com>
2023-10-02 14:25:22 -0300

    Feat: botão Mais notificações aparece apenas quando as notificações aparecerem.

--- src/components/Pages/PageUser/PageUserNotification/PageUserNotificationsHistoric.tsx
+++ src/components/Pages/PageUser/PageUserNotification/PageUserNotificationsHistoric.tsx
@@ -51,12 +51,14 @@ export default function PageNotificationHistoric({
         ) : (
           <NotificationCard.Skeleton quantity={8} />
         )}
-        <Common.Button
-          onClick={onClick}
-          className="border-none text-mesh-color-neutral-200"
-        >
-          Exibir mais notificações...
-        </Common.Button>
+        {!loading && (
+          <Common.Button
+            onClick={onClick}
+            className="border-none text-mesh-color-neutral-200"
+          >
+            Exibir mais notificações...
+          </Common.Button>
+        )}
       </div>
     </div>
   )
