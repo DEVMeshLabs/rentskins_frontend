fe02f71a05e95ab36e61ab938a43f4aec5239636
Gabriel Almeida <viniciusgsa99@gmail.com>
2023-10-03 22:47:36 -0300

    Commit

--- src/components/Pages/PageUser/PageUserNotification/PageUserNotificationsTransaction.tsx
+++ src/components/Pages/PageUser/PageUserNotification/PageUserNotificationsTransaction.tsx
@@ -60,7 +60,7 @@ export default function PageNotificationTransaction({
           className="mt-4 flex h-[24rem] w-full scroll-p-24 flex-col gap-4 overflow-y-scroll pr-2"
           data-aos="fade-up"
         >
-          {data ? renderPending : <TransactionCard.Skeleton quantity={3} />}
+          {!loading ? renderPending : <TransactionCard.Skeleton quantity={3} />}
         </div>
       </div>
       <div
