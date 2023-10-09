63579f4a0e1b9b197a14a68e4aff93af12b42b3c
Tiago Braga Costa <ztiagok@gmail.com>
2023-10-09 12:22:49 -0300

    refactor: Corrigindo props da TransactionsImage

--- src/components/Others/TransactionCard/TransactionImage.tsx
+++ src/components/Others/TransactionCard/TransactionImage.tsx
@@ -17,11 +17,9 @@ export function TransactionImage({
     <Image
       src={image}
       alt={alt}
-      width={112}
-      height={64}
-      className="w-28 rounded-lg bg-mesh-color-others-black p-4"
       width={width}
       height={height}
+      className="w-28 rounded-lg bg-mesh-color-others-black p-4"
     />
   )
 }
