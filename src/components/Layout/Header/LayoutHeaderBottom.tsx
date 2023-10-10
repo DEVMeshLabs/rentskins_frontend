4079397bc2a912513af4fa06445546b3a2b6cdc6
Gabriel Almeida <viniciusgsa99@gmail.com>
2023-10-10 16:10:07 -0300

    Fix: corrigindo gramática.

--- src/components/Layout/Header/LayoutHeaderBottom.tsx
+++ src/components/Layout/Header/LayoutHeaderBottom.tsx
@@ -150,7 +150,7 @@ export const ContainerItem = ({
     Pistolas: 'Pistol',
     Luvas: 'Glove',
     Figurinhas: 'Sticker',
-    Agentes: 'Agent',
+    Agentes: 'Agente',
     Pesadas: 'Heavy',
   }
 
