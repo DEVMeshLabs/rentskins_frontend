96cc4046755974c38c87045c73159a8b1fb50af5
Tiago Braga Costa <ztiagok@gmail.com>
2023-10-26 14:33:05 -0300

    refactor: Melhorando o CSS dos cards e renderizando o inventário corretamente

--- src/components/Others/CardSkin/CardSkinInventory.tsx
+++ src/components/Others/CardSkin/CardSkinInventory.tsx
@@ -8,7 +8,6 @@ import SkinService from '@/services/skin.service'
 import useComponentStore from '@/stores/components.store'
 import useFilterStore from '@/stores/filters.store'
 import useSkinsStore from '@/stores/skins.store'
-import { filterSkinsToInventory } from '@/utils/filterSkinsToInvetory'
 import { useQuery } from '@tanstack/react-query'
 import classNames from 'classnames'
 import { useSession } from 'next-auth/react'
@@ -28,45 +27,46 @@ export function CardSkinInventory() {
   const { data: skinsProfile, refetch: refetchSkinsProfile } = useQuery({
     queryKey: ['profileSkins', trueSession?.user?.steam?.steamid!],
     queryFn: () =>
-      SkinService.findAllSkinsByIdSeller(
-        trueSession?.user?.steam?.steamid!,
-        page,
-      ),
+      SkinService.findAllSkinsByIdSeller(trueSession?.user?.steam?.steamid!),
     keepPreviousData: true,
   })
 
   const { data, isLoading, isRefetching, refetch } = useQuery({
     queryKey: ['skinsInventory'],
     queryFn: async () =>
-      SkinService.findBySkinsInventoryWithFilters(
-        '76561198862407248',
-        // trueSession.user?.steam?.steamid!,
+      await SkinService.findBySkinsInventory(
+        trueSession.user?.steam?.steamid!,
         trueSession.user?.token!,
-        inventoryTypeFilter,
-        Number(page),
-        Number(16),
       ),
     enabled: status === 'authenticated',
     keepPreviousData: true,
   })
 
   useEffect(() => {
-    if (
-      data?.data &&
-      data.data.inventory &&
-      skinsProfile?.data &&
-      skinsProfile.data.skins
-    ) {
+    if (data?.data && skinsProfile?.data && skinsProfile.data.skins) {
       setSteamItens(
-        filterSkinsToInventory(data.data.inventory, skinsProfile.data.skins),
+        data?.data.filter(
+          ({ assetid }: any) =>
+            !skinsProfile?.data?.skins.some(
+              ({ asset_id }) => asset_id === assetid,
+            ),
+        ),
       )
     }
   }, [data, skinsProfile])
 
+  console.log(data?.data?.length)
+  console.log(skinsProfile?.data?.skins.length)
+  console.log(steamItens.length)
+
+  const maxPages = Math.ceil(steamItens.length / 16)
+
+  console.log(maxPages)
+
   useEffect(() => {
     refetch()
     refetchSkinsProfile()
-  }, [page, inventoryTypeFilter, refetch])
+  }, [page, inventoryTypeFilter, refetch, refetchSkinsProfile])
 
   useEffect(() => {
     setIsInventoryFetching(isLoading || isRefetching)
@@ -127,111 +127,114 @@ export function CardSkinInventory() {
               },
               index: number,
             ) => {
-              const primeiroName = name.split('|')[0]
-              const statusFloatText = market_name.match(/\((.*?)\)/g)
-              const statusFloat =
-                statusFloatText && statusFloatText[0].replace(/\(|\)/g, '')
-              const itemIsAWeapon =
-                !tags[0].name.includes('Sticker') &&
-                !tags[0].name.includes('Agent')
-              const completeType = type.split(' ')
-              const category = completeType.pop()!
-              const rarity = tags.filter(
-                ({ category, name }) => category === 'Rarity' && name,
-              )[0].name
-              const weapon = name.includes('Gloves')
-                ? tags[0].name
-                : tags[1].name
-              const isSelected = skinsToAdvertise.some(
-                ({ id }) => assetid === id,
-              )
-              const categoryType = tags.filter(
-                ({ category }) => category === 'Type',
-              )
-              const isRentable = !(
-                categoryType[0].name === 'Graffiti' ||
-                categoryType[0].name === 'Container' ||
-                categoryType[0].name === 'Sticker' ||
-                categoryType[0].name === 'Collectible'
-              )
-              const linkForPreviewSkin = actions ? actions[0].link : '#'
+              if (index < page * 16 && index >= page * 16 - 16) {
+                const primeiroName = name.split('|')[0]
+                const statusFloatText = market_name.match(/\((.*?)\)/g)
+                const statusFloat =
+                  statusFloatText && statusFloatText[0].replace(/\(|\)/g, '')
+                const itemIsAWeapon =
+                  !tags[0].name.includes('Sticker') &&
+                  !tags[0].name.includes('Agent')
+                const completeType = type.split(' ')
+                const category = completeType.pop()!
+                const rarity = tags.filter(
+                  ({ category, name }) => category === 'Rarity' && name,
+                )[0].name
+                const weapon = name.includes('Gloves')
+                  ? tags[0].name
+                  : tags[1].name
+                const isSelected = skinsToAdvertise.some(
+                  ({ id }) => assetid === id,
+                )
+                const categoryType = tags.filter(
+                  ({ category }) => category === 'Type',
+                )
+                const isRentable = !(
+                  categoryType[0].name === 'Graffiti' ||
+                  categoryType[0].name === 'Container' ||
+                  categoryType[0].name === 'Sticker' ||
+                  categoryType[0].name === 'Collectible'
+                )
+                const linkForPreviewSkin = actions ? actions[0].link : '#'
 
-              return (
-                <ModalSkinShowcaseMain
-                  isRentable={isRentable}
-                  key={assetid}
-                  asset_id={assetid}
-                  skinImage={icon_url}
-                  marketName={market_hash_name}
-                  skinName={market_name}
-                  skinCategory={category}
-                  skinWeapon={weapon}
-                  statusFloat={statusFloat || ''}
-                  skinRarity={rarity}
-                  float={'0.2555'}
-                  linkForPreviewSkin={linkForPreviewSkin}
-                  linkForProfile={trueSession.user?.steam?.profileurl!}
-                  id={assetid}
-                  isSelected={isSelected}
-                  activator={
-                    <div
-                      className={classNames(
-                        'group relative w-[206px] cursor-pointer gap-2 rounded-lg border-[1px] border-mesh-color-neutral-600 border-opacity-60 px-3 pb-4 pt-3 text-white transition-all hover:bg-mesh-color-neutral-700',
-                        {
-                          'bg-mesh-color-neutral-700': isSelected,
-                        },
-                      )}
-                    >
+                return (
+                  <ModalSkinShowcaseMain
+                    isRentable={isRentable}
+                    key={assetid}
+                    asset_id={assetid}
+                    skinImage={icon_url}
+                    marketName={market_hash_name}
+                    skinName={market_name}
+                    skinCategory={category}
+                    skinWeapon={weapon}
+                    statusFloat={statusFloat || ''}
+                    skinRarity={rarity}
+                    float={'0.2555'}
+                    linkForPreviewSkin={linkForPreviewSkin}
+                    linkForProfile={trueSession.user?.steam?.profileurl!}
+                    id={assetid}
+                    isSelected={isSelected}
+                    activator={
                       <div
                         className={classNames(
-                          'absolute left-1 top-1 z-10 h-6 w-6 rounded-full border-[1px] border-mesh-color-neutral-400 transition-all',
+                          'group relative w-[206px] cursor-pointer gap-2 rounded-lg border-[1px] border-mesh-color-neutral-600 border-opacity-60 px-3 pb-4 pt-3 text-white transition-all hover:bg-mesh-color-neutral-700',
                           {
-                            'border-mesh-color-neutral-100 bg-mesh-color-accent-1100':
-                              isSelected,
-                          },
-                          {
-                            'group-hover:border-mesh-color-neutral-200 group-hover:bg-mesh-color-neutral-500':
-                              !isSelected,
+                            'bg-mesh-color-neutral-700': isSelected,
                           },
                         )}
-                      />
-                      <CardSkin.Root classname="flex relative flex-col h-[245px] justify-between">
-                        <div className="h-full">
-                          <CardSkin.Image
-                            icon_url={icon_url}
-                            rarity={rarity}
-                            primeiroName={primeiroName}
-                          />
-                          {isSelected && (
-                            <Common.Button className="absolute left-11 top-[85px] w-1/2 border-none bg-mesh-color-neutral-500 opacity-100">
-                              <Common.Title bold={600}>Alterar</Common.Title>
-                            </Common.Button>
+                      >
+                        <div
+                          className={classNames(
+                            'absolute left-1 top-1 z-10 h-6 w-6 rounded-full border-[1px] border-mesh-color-neutral-400 transition-all',
+                            {
+                              'border-mesh-color-neutral-100 bg-mesh-color-accent-1100':
+                                isSelected,
+                            },
+                            {
+                              'group-hover:border-mesh-color-neutral-200 group-hover:bg-mesh-color-neutral-500':
+                                !isSelected,
+                            },
                           )}
-                          <CardSkin.Content
-                            market_name={market_name}
-                            primeiroName={primeiroName}
-                            float={
-                              itemIsAWeapon && isRentable ? '0.254665' : ''
-                            }
-                          />
-                        </div>
-                        {itemIsAWeapon && isRentable && (
-                          <ColoredLine position={0.254665} />
-                        )}
-                      </CardSkin.Root>
-                    </div>
-                  }
-                />
-              )
+                        />
+                        <CardSkin.Root classname="flex relative flex-col h-[245px] justify-between">
+                          <div className="h-full">
+                            <CardSkin.Image
+                              icon_url={icon_url}
+                              rarity={rarity}
+                              primeiroName={primeiroName}
+                            />
+                            {isSelected && (
+                              <Common.Button className="absolute left-11 top-[85px] w-1/2 border-none bg-mesh-color-neutral-500 opacity-100">
+                                <Common.Title bold={600}>Alterar</Common.Title>
+                              </Common.Button>
+                            )}
+                            <CardSkin.Content
+                              market_name={market_name}
+                              primeiroName={primeiroName}
+                              float={
+                                itemIsAWeapon && isRentable ? '0.254665' : ''
+                              }
+                            />
+                          </div>
+                          {itemIsAWeapon && isRentable && (
+                            <ColoredLine position={0.254665} />
+                          )}
+                        </CardSkin.Root>
+                      </div>
+                    }
+                  />
+                )
+              }
+              return null
             },
           )
         ) : (
           renderEmptyMessage()
         )}
       </div>
-      {!isLoading && data?.data && data?.data.maxPages > 0 && (
+      {!isLoading && data?.data && maxPages > 0 && (
         <LayoutPagination
-          maxPages={data.data.maxPages}
+          maxPages={maxPages}
           pageState={page}
           setPageState={setPage}
           disabled={isLoading || isRefetching}
--- src/services/skin.service.ts
+++ src/services/skin.service.ts
@@ -22,13 +22,19 @@ export default class SkinService {
     return Api.get<ISkins[]>(`/skins/weapon/${weapon}`)
   }
 
-  public static findBySkinsInventory(steamid: string, token: string) {
-    return Api.get<IInventory>(`/skins/inventory/${steamid}`, {
+  public static async findBySkinsInventory(steamid: string, token: string) {
+    console.log(steamid)
+    console.log(token)
+    const response = await Api.get<IInventory>(`/skins/inventory/${steamid}`, {
       headers: { Authorization: 'Bearer ' + token },
     })
+      .then((response) => response)
+      .catch((e) => e)
+
+    return response
   }
 
-  public static findBySkinsInventoryWithFilters(
+  public static findBySkinsInventoryWithPages(
     steamid: string,
     token: string,
     filterType?: string[],
