7cba7edcca7649015c5c77b2570f430a78d996c0
Tiago Braga Costa <ztiagok@gmail.com>
2023-11-08 13:55:25 -0300

    feat: Adicionando dois testes na <Home />

--- src/app/home/home.spec.tsx
+++ src/app/home/home.spec.tsx
@@ -1,15 +1,57 @@
 import { TestTools } from '@/tools/tests/testTools.tool'
+import { useQuery } from '@tanstack/react-query'
+import { screen } from '@testing-library/react'
 import Home from './page'
 
-TestTools.startNextAuth()
+jest.mock('next-auth/react', () => {
+  const originalModule = jest.requireActual('next-auth/react')
+  const mockSession = TestTools.nextAuthSession()
+  return {
+    __esModule: true,
+    ...originalModule,
+    useSession: jest.fn(() => {
+      return { data: mockSession, status: 'authenticated' }
+    }),
+  }
+})
+
+export function useCustomHook() {
+  return useQuery({
+    queryKey: ['allSkins'],
+    queryFn: () => 'Hello',
+    retry: false,
+  })
+}
 
 describe('<Home />', () => {
-  it('Deve renderizar o título principal.', async () => {
-    const container = TestTools.customRender(<Home />)
+  beforeEach(() => {
+    TestTools.customRender(<Home />)
+  })
 
-    const topTitle = container.getByText(/Descubra o mundo das skins de/i)
-    const bottomTitle = container.getByText(/Counter-Strike/i)
+  it('Deverá renderizar o título principal.', async () => {
+    // const container = TestTools.customRender(<Home />)
+
+    const topTitle = screen.getByText(/Descubra o mundo das skins de/i)
+    const bottomTitle = screen.getByText(/Counter-Strike/i)
 
     expect(topTitle && bottomTitle).toBeTruthy()
   })
+
+  it('Deverá renderizar a imagem de fundo correta.', () => {
+    const image = screen.getByTestId('home-hero')
+
+    expect(image).toBeDefined()
+    expect(image.className).toContain(
+      'bg-mesh-image-hero' && 'bg-cover' && 'bg-center' && 'bg-no-repeat',
+    )
+  })
+
+  // it('Deverá renderizar a lista de itens.', async () => {
+  //   // const container = TestTools.customRender(<Home />)
+  //   const container = TestTools.customRender(<Home />)
+
+  //   const { result } = renderHook(() => useCustomHook())
+
+  //   await (await waitFor(() => expect(result.current.isSuccess))).toBe(true)
+  // })
 })
--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -31,7 +31,10 @@ export default function Home({ searchParams }: IProps) {
   return (
     <main className="h-full">
       <div className="h-screen">
-        <div className="flex h-4/6 flex-col items-center justify-center bg-mesh-image-hero bg-cover bg-center bg-no-repeat">
+        <div
+          data-testid="home-hero"
+          className="flex h-4/6 flex-col items-center justify-center bg-mesh-image-hero bg-cover bg-center bg-no-repeat"
+        >
           <PageHomeHero />
         </div>
         <div className="h-1/5 w-full bg-mesh-color-neutral-800">
