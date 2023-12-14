49a703eaf0d41170bda6085771e7a5c215a5012a
Tiago Braga Costa <ztiagok@gmail.com>
2023-12-14 15:58:09 -0300

    refactor: Corrigindo informações de status do perfil

--- src/components/Pages/PageProfile/PageProfileSelf.tsx
+++ src/components/Pages/PageProfile/PageProfileSelf.tsx
@@ -62,8 +62,11 @@ export default function PageProfileSelf() {
   )!.getFullYear()}`
 
   const deliveryFee =
-    user?.data?.total_exchanges_completed && user?.data?.total_exchanges
-      ? (user?.data?.total_exchanges_completed / user?.data?.total_exchanges) *
+    user?.data?.total_exchanges_completed! > 0 ||
+    user?.data?.total_exchanges_failed! > 0
+      ? (user?.data?.total_exchanges_completed! /
+          (user?.data?.total_exchanges_completed! +
+            user?.data?.total_exchanges_failed!)) *
         100
       : 'Sem informações'
 
@@ -72,7 +75,10 @@ export default function PageProfileSelf() {
       <ModalReturnMain />
       {status === 'authenticated' && user?.data ? (
         <PerfilPerson
-          totalExchanges={user?.data?.total_exchanges}
+          totalExchanges={
+            user?.data?.total_exchanges_completed +
+            user?.data?.total_exchanges_failed
+          }
           deliveryTime={user?.data?.delivery_time}
           deliveryFee={deliveryFee}
           isLoading={isLoadingUser}
--- src/tools/time.tool.ts
+++ src/tools/time.tool.ts
@@ -1,15 +1,13 @@
 export default class Time {
   public static roundTime(time: string) {
+    if (time === 'Sem informações') return 'Sem informações'
+
     const [horas, minutos, segundos] = time.split(':').map(Number)
 
     const segundosArredondados = Math.round(segundos)
     const horasFormatadas = horas?.toString().padStart(2, '0')
     const minutosFormatados = minutos?.toString().padStart(2, '0')
     const segundosFormatados = segundosArredondados?.toString().padStart(2, '0')
-
-    if (!horas || !minutos || !segundos) {
-      return 'Sem informações'
-    }
 
     return `${horasFormatadas}:${minutosFormatados}:${segundosFormatados}`
   }
