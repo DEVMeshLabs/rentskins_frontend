fe04ab5a4aee4a652f61fbb55478b19a3c6942c3
Tiago Braga Costa <ztiagok@gmail.com>
2024-02-02 15:57:54 -0300

    refactor: Arrumando a porcentagem do float

--- src/components/Others/ColoredLine/index.tsx
+++ src/components/Others/ColoredLine/index.tsx
@@ -17,11 +17,11 @@ export default function ColoredLine({ position }: IProps) {
         <IconLittleArrow />
       </div>
       <div className="relative flex w-full items-center rounded-sm">
-        <div className="h-[6px] w-[10%] rounded-l-md bg-[#1E9A04]" />
-        <div className="h-[6px] w-[13%] bg-[#59C675]" />
-        <div className="h-[6px] w-[17%] bg-[#FADE5E]" />
-        <div className="h-[6px] w-[30%] bg-[#E84E6A]" />
-        <div className="h-[6px] w-[30%] rounded-r-md bg-[#CA1733]" />
+        <div className="h-[6px] w-[7%] rounded-l-md bg-[#1E9A04]" />
+        <div className="h-[6px] w-[7%] bg-[#59C675]" />
+        <div className="h-[6px] w-[23%] bg-[#FADE5E]" />
+        <div className="h-[6px] w-[7%] bg-[#E84E6A]" />
+        <div className="h-[6px] w-[55%] rounded-r-md bg-[#CA1733]" />
       </div>
     </div>
   )
