2676b76cc23e617314eb86bc9cd68f8d0ca713d8
Gabriel Almeida <viniciusgsa99@gmail.com>
2024-02-22 22:17:12 -0300

    Correções.

--- src/components/Layout/Header/LayoutHeaderTop.tsx
+++ src/components/Layout/Header/LayoutHeaderTop.tsx
@@ -94,11 +94,9 @@ export function LayoutHeaderTop() {
     userConfig!.data?.url_trade !== ''
 
   useEffect(() => {
-    // const interval = setInterval(() => {
     if (trueSession?.user?.steam?.steamid) {
       refetch() // Refaz a requisição a cada 1 segundo
     }
-    // }, 10 * 60 * 1000)
     setHasNotifications(Notifications.hasNotification(data?.data))
   }, [pathname])
 
