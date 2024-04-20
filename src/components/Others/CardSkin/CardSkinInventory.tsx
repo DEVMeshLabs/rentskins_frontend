46fdd7c07ed763ee4aaf3655f6df7cab4d215b61
Tiago Braga Costa <ztiagok@gmail.com>
2024-04-19 21:21:11 -0300

    refactor: Pull origin

--- src/components/Others/CardSkin/CardSkinInventory.tsx
+++ src/components/Others/CardSkin/CardSkinInventory.tsx
@@ -144,8 +144,6 @@ export function CardSkinInventory({ apiKey }: Props) {
     )
   }
 
-  console.log(itemsLeftOnInventory)
-
   return (
     <div className="flex flex-col items-center justify-center">
       <div className="ml-2 flex flex-wrap justify-center gap-4">
